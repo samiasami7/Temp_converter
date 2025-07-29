b72754a7ff8f3e93d8da23ae2bfb0ba23fa3eeda
samia <samia@SAMIA>
2025-07-29 22:21:32 +0600

    Basic Add

--- Final-project/src/Temp_converter.java
+++ Final-project/src/Temp_converter.java
@@ -1,9 +1,27 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Temp_converter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		JFrame f = new JFrame();
+		f.setTitle("Temperature Converter");
+		f.setSize(450, 400);
+		f.setLayout(null);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		JLabel l1 = new JLabel("Enter Temperature:");
+        l1.setBounds(20, 20, 150, 25);
+        f.add(l1);
+		
+        
+        
+		f.setVisible(true);
+		
 	}
 
 }
